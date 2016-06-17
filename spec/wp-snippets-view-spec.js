523984268889ffa5f40eb88c66931f71becef952
Chris Wilcoxson <chris@slushman.com>
2016-06-17 08:42:07 -0500

    Initial commit.

---
+++ spec/wp-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WpSnippetsView from '../lib/wp-snippets-view';
+
+describe('WpSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
