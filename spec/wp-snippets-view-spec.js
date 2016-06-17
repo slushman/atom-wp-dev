b0337d8c880cfe6568707ee8b75cfdc3901d8e00
Chris Wilcoxson <chris@slushman.com>
2016-06-17 10:45:38 -0500

    Removed extra files. Added all Customizer snippets.

--- spec/wp-snippets-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import WpSnippetsView from '../lib/wp-snippets-view';
-
-describe('WpSnippetsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
